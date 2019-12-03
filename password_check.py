64711618aed4002bcf73b1c62b54ac3a28478384
maimumatsumoto <maimu.matsumoto@my.jcu.edu.au>
2019-12-03 18:57:23 +0800

    created a def get_password() function and def print_asterisks() function so that only one thing happens in a function.

--- password_check.py
+++ password_check.py
@@ -1,8 +1,22 @@
-password = input("Enter password")
+def main():
 
-while len(password)<7:
-    print("too short")
-    password = input("enter pass again")
+    password = get_password()
 
-for i in range(0,len(password)):
-    print("*", end='')
+    while len(password)<7:
+        print("too short")
+        password = input("enter pass again")
+
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    for i in range(0, len(password)):
+        print("*", end='')
+
+
+def get_password():
+    password = input("Enter password")
+    return password
+
+
+main()
