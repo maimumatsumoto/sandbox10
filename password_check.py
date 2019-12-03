264d489cbcb809a866db1af3dd13370b88b8ee34
maimumatsumoto <maimu.matsumoto@my.jcu.edu.au>
2019-12-03 19:01:57 +0800

    change back to the original version

--- password_check.py
+++ password_check.py
@@ -1,22 +1,10 @@
-def main():
-
-    password = get_password()
-
-    while len(password)<7:
-        print("too short")
-        password = input("enter pass again")
-
-    print_asterisks(password)
+password = input("Enter password")
 
 
-def print_asterisks(password):
-    for i in range(0, len(password)):
-        print("*", end='')
 
+while len(password)<7:
+    print("too short")
+    password = input("enter pass again")
 
-def get_password():
-    password = input("Enter password")
-    return password
-
-
-main()
+for i in range(0,len(password)):
+    print("*", end='')
